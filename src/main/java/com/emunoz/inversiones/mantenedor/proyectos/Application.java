20fed0681d5782306089b75a744d1f095b44df16
kenomun <eugenio.mun.f@gmail.com>
2023-12-07 18:26:16 +0200

    se crea la estructura del proyecto servicio y controlador que permite buscar un listado o por id

--- src/main/java/com/emunoz/inversiones/mantenedor/proyectos/Application.java
+++ src/main/java/com/emunoz/inversiones/mantenedor/proyectos/Application.java
@@ -1,9 +1,11 @@
-package com.emunoz.inversiones.acceso;
+package com.emunoz.inversiones.mantenedor.proyectos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.emunoz.inversiones.mantenedor.proyectos")
 public class Application {
 
 	public static void main(String[] args) {
